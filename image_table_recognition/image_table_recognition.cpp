c1beee6250d7640f7a09d61679cd50881f7e32ca
jingsupo <jingsupo@outlook.com>
2022-12-29 15:05:50 +0800

    refine

--- image_table_recognition/image_table_recognition.cpp
+++ image_table_recognition/image_table_recognition.cpp
@@ -12,7 +12,6 @@
 #define GetCurrentDir getcwd
 #else
 #endif
-#include <fstream>
 
 ///处理流程：
 ///1、图像灰度化处理
@@ -29,26 +28,28 @@ private:
     int params_margin_x = 10;   //x轴点达到一定的距离，才算两个有效点，确定线的个数
     int params_margin_y = 10;   //y轴点达到一定的距离，才算两个有效点，确定线的个数
     int params_dot_margin = 10; //和平均线的偏移量（对缝隙起作用，可去除点，也可变为独立一个点）
-    int params_line_x = 10;     //x上点个数的差值调节（线不均匀，有的粗有的细，甚至有的不连续）
-    int params_line_y = 10;     //y上点个数的差值调节（线不均匀，有的粗有的细，甚至有的不连续）
+    int params_line_x = 20;     //x上点个数的差值调节（线不均匀，有的粗有的细，甚至有的不连续）
+    int params_line_y = 20;     //y上点个数的差值调节（线不均匀，有的粗有的细，甚至有的不连续）
 
 private:
     std::pair<std::vector<int>, std::vector<int>> _where(cv::Mat mat)
     {
         std::vector<int> x;
         std::vector<int> y;
+
         cv::Size size = mat.size();
-        for (int i = 0; i < size.width; i++)
+        for (int h = 0; h < size.height; h++)
         {
-            for (int j = 0; j < size.height; j++)
+            for (int w = 0; w < size.width; w++)
             {
-                if (mat.at<int>(i, j) > 0)
+                if (mat.at<int>(w, h) > 0)
                 {
-                    x.push_back(i);
-                    y.push_back(j);
+                    x.push_back(h);
+                    y.push_back(w);
                 }
             }
         }
+
         return std::make_pair(x, y);
     }
 
@@ -71,23 +72,25 @@ private:
         int x = va_arg(arg_ptr, int);
         //将va_list类型的指针复位，清空可变参数列表
         va_end(arg_ptr);
+
         auto yx_in = [=](std::vector<int> v) { return (v[0] == y) and (v[1] == x); };
+
         return std::find_if(vec.begin(), vec.end(), yx_in) != vec.end();
     }
 
     int _index(std::vector<int> vec, int val)
     {
         auto it = std::find(vec.begin(), vec.end(), val);
-        if (it != vec.end())
-        {
-            return std::distance(vec.begin(), it);
-        }
+
+        if (it != vec.end()) return std::distance(vec.begin(), it);
+
         return -1;
     }
 
     int _recognize_line(std::vector<int> line_xs, std::vector<int> line1, std::vector<int> line2, int num, int num1, int num2)
     {
         std::vector<int> line_list;
+
         for (auto& k : { -3, -2, -1, 0, 1, 2, 3 })
         {
             for (int i = 0; i < line_xs.size(); i++)
@@ -101,6 +104,7 @@ private:
                 }
             }
         }
+
         return line_list.size();
     }
 
@@ -111,19 +115,19 @@ private:
             std::cout << "表格交点数为 0\n";
             return std::pair<std::vector<int>, std::vector<int>>();
         }
+
         std::vector<int> coord_list;
-        int i = 0;
         std::vector<int> sorted_coords(coords.size());
         std::copy(coords.begin(), coords.end(), sorted_coords.begin());
         std::sort(sorted_coords.begin(), sorted_coords.end());
+
+        int i = 0;
         for (; i < sorted_coords.size() - 1; i++)
         {
-            if (sorted_coords[static_cast<std::vector<int, std::allocator<int>>::size_type>(i) + 1] - sorted_coords[i] > params_margin)
-            {
-                coord_list.push_back(sorted_coords[i]);
-            }
+            if (sorted_coords[i + 1] - sorted_coords[i] > params_margin) coord_list.push_back(sorted_coords[i]);
         }
         coord_list.push_back(sorted_coords[i]);//包括最后一点，每个点都是取最大的一个
+
         return std::make_pair(sorted_coords, coord_list);
     }
 };
@@ -150,9 +154,22 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
             out_path = std::string(buff) + "/" + out_path;
         }
     }
+
     if (_access(out_path.c_str(), 0) == -1)
     {
         if (_mkdir(out_path.c_str()) == -1)
+        {
+            std::cout << "文件夹创建失败\n";
+            return std::vector<std::vector<int>>();
+        }
+    }
+
+    std::string img_name = std::to_string(rand());
+    std::string save_dir = out_path + "/" + img_name;
+
+    if (_access(save_dir.c_str(), 0) == -1)
+    {
+        if (_mkdir(save_dir.c_str()) == -1)
         {
             std::cout << "文件夹创建失败\n";
             return std::vector<std::vector<int>>();
@@ -166,30 +183,40 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
         std::cout << "图像读取失败\n";
         return std::vector<std::vector<int>>();
     }
+
     //灰度化处理
     cv::Mat gray;
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+
     //二值化处理
     cv::Mat binary;
     cv::adaptiveThreshold(~gray, binary, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 15, -10);
+
     cv::imwrite(out_path + "/cell.png", binary);
+
     //识别横线
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(25, 1));
     cv::Mat erode;
     cv::erode(binary, erode, kernel);//腐蚀
     cv::Mat horizontal_line;
     cv::dilate(erode, horizontal_line, kernel);//膨胀
+
     cv::imwrite(out_path + "/horizontal_line.png", horizontal_line);
+
     //识别竖线
     kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 25));
     cv::erode(binary, erode, kernel);//腐蚀
     cv::Mat vertical_line;
     cv::dilate(erode, vertical_line, kernel);//膨胀
+
     cv::imwrite(out_path + "/vertical_line.png", vertical_line);
+
     //识别表格
     cv::Mat table;
     cv::add(horizontal_line, vertical_line, table);
+
     cv::imwrite(out_path + "/table.png", table);
+
     //获取表格中所有的线
     auto lines = _where(table);
     std::vector<int> line_ys = lines.first;
@@ -200,10 +227,13 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
     std::cout << "line_xs size: " << line_xs.size() << "\n";
     maxPosition = std::max_element(line_xs.begin(), line_xs.end());
     std::cout << "line_xs max: " << *maxPosition << " at the postion of " << maxPosition - line_xs.begin() << "\n";
+
     //识别交点
     cv::Mat intersecting_point;
     cv::bitwise_and(horizontal_line, vertical_line, intersecting_point);
+
     cv::imwrite(out_path + "/intersecting_point.png", intersecting_point);
+
     //获取表格交点坐标
     auto xys = _where(intersecting_point);
     std::vector<int> ys = xys.first;
@@ -234,6 +264,7 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
     //根据交点集获取单元格轮廓并进行过滤
     std::vector<std::vector<int>> coord_list;
     std::map<int, std::vector<std::vector<int>>> coord_map;
+
     for (int i = 0; i < sorted_ys.size(); i++)
     {
         for (auto y = list_y.begin(); y != list_y.end(); y++)
@@ -249,68 +280,52 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
             }
         }
     }
+
     for (int i = 0; i < list_y.size(); i++)
     {
         std::vector<std::vector<int>> line_list;
+
         for (auto coord = coord_list.begin(); coord != coord_list.end(); coord++)
         {
-            if ((*coord)[0] == list_y[i])
-            {
-                line_list.push_back(*coord);
-            }
+            if ((*coord)[0] == list_y[i]) line_list.push_back(*coord);
         }
+
         std::sort(line_list.begin(), line_list.end(), [](std::vector<int> v1, std::vector<int> v2) { return v1[1] < v2[1]; });
-        std::cout << "888888\n";
-        std::cout << line_list.size() << "\n";
+
         coord_map[i] = line_list;
     }
+
     std::vector<std::vector<int>> cells_coords;//单元格坐标
+
     for (int i = 0; i < coord_map.size() - 1; i++)
     {
-        std::cout << "777777\n";
-        std::cout << coord_map[i].size() << "\n";
         for (int index = 0; index < coord_map[i].size(); index++)
         {
-            if (index == coord_map[i].size() - 1)
-            {
-                break;
-            }
+            if (index == coord_map[i].size() - 1) break;
+
             int cell_up = coord_map[i][index][0];//单元格上边框
             int cell_left = coord_map[i][index][1];//单元格左边框
+
             for (int j = 1; j < coord_map[i].size(); j++)
             {
                 int m = i;
                 int n = index + j;
                 int mark_num = 0;
-                if (n == coord_map[i].size())
-                {
-                    break;
-                }
+
+                if (n == coord_map[i].size()) break;
+
                 int cell_down = coord_map[m + 1][0][0];//单元格下边框
                 int cell_right = coord_map[i][n][1];//单元格右边框
+
                 while (m <= coord_map.size() - 2)
                 {
                     std::vector<int> tmp;
+
                     for (auto& coord : coord_map[m + 1])
                     {
                         tmp.push_back(coord[1]);
                     }
-                    //std::cout
-                    //    << _in_vec(tmp, cell_left)
-                    //    << _in_vec(tmp, cell_right)
-                    //    << " "
-                    //    << std::abs(_recognize_line(line_xs, line_xs, line_ys, cell_left, cell_up, cell_down) - (cell_down - cell_up))
-                    //    //<= params_line_y
-                    //    << " "
-                    //    << std::abs(_recognize_line(line_xs, line_xs, line_ys, cell_right, cell_up, cell_down) - (cell_down - cell_up))
-                    //    //<= params_line_y
-                    //    << " "
-                    //    << std::abs(_recognize_line(line_xs, line_ys, line_xs, cell_up, cell_left, cell_right) - (cell_right - cell_left))
-                    //    //<= params_line_x
-                    //    << " "
-                    //    << std::abs(_recognize_line(line_xs, line_ys, line_xs, cell_down, cell_left, cell_right) - (cell_right - cell_left))
-                    //    //<= params_line_x
-                    //    << "\n";
+
                     if (_in_vec(tmp, cell_left) and _in_vec(tmp, cell_right) and
                         (std::abs(_recognize_line(line_xs, line_xs, line_ys, cell_left, cell_up, cell_down) - (cell_down - cell_up))
                             <= params_line_y) and
@@ -321,12 +336,8 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
                         (std::abs(_recognize_line(line_xs, line_ys, line_xs, cell_down, cell_left, cell_right) - (cell_right - cell_left))
                             <= params_line_x))
                     {
-                        std::cout << "666666\n";
                         mark_num = 1;
-                        cv::Mat roi1 = img.rowRange(cell_up, cell_down);
-                        std::cout << img.size() << " " << img.dims << "\n";
-                        std::cout << roi1.size() << " " << roi1.dims << "\n";
-                        cv::Mat roi2 = roi1.colRange(cell_left, cell_right);
+
                         std::vector<int> coords = { cell_up, cell_down, cell_left, cell_right };
                         cells_coords.push_back(coords);
 
@@ -335,22 +346,16 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
                         int order_num3 = _index(list_y, cell_left);
                         int order_num4 = _index(list_y, cell_right) - 1;
 
-                        std::string img_name = std::to_string(rand());
-                        std::string save_dir = out_path + "/" + img_name;
-                        if (_access(save_dir.c_str(), 0) == -1)
-                        {
-                            if (_mkdir(save_dir.c_str()) == -1)
-                            {
-                                std::cout << "文件夹创建失败\n";
-                                continue;
-                            }
-                        }
                         std::string new_img_name = (img_name + "_" +
                             std::to_string(order_num1) + "_" +
                             std::to_string(order_num2) + "_" +
                             std::to_string(order_num3) + "_" +
                             std::to_string(order_num4) + ".jpg");
                         std::string save_path = save_dir + "/" + new_img_name;
+
+                        cv::Mat roi1 = img.rowRange(cell_up, cell_down);
+                        cv::Mat roi2 = roi1.colRange(cell_left, cell_right);
+
                         cv::imwrite(save_path, roi2);
 
                         break;
@@ -360,19 +365,20 @@ std::vector<std::vector<int>> image_table_recognition::run(std::string img_path,
                         m++;
                     }
                 }
-                if (mark_num == 1)
-                {
-                    break;
-                }
+
+                if (mark_num == 1) break;
             }
         }
     }
+
     return cells_coords;
 }
 
 int main()
 {
     image_table_recognition itr;
-    itr.run("qiaofeng.png", "");
-    //system("pause");
+    auto ret = itr.run("qiaofeng.png", "");
+    std::cout << "单元格数量：" << ret.size() << "\n";
+    std::for_each(ret.begin(), ret.end(), [](std::vector<int> v) { std::cout << "单元格坐标：" << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << "\n"; });
+    system("pause");
 }
